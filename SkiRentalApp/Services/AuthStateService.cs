555fbd77e9b9d02ea50f1248af0c05e9679cf8c7
Andy <andy-tux@aon.at>
2025-05-23 14:49:58 +0200

    Changed to protectedsessionstorage for saving the login

--- SkiRentalApp/Services/AuthStateService.cs
+++ SkiRentalApp/Services/AuthStateService.cs
@@ -1,29 +1,60 @@
-﻿namespace SkiRentalApp.Services
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace SkiRentalApp.Services
 {
 	public class AuthStateService
 	{
-		public bool IsLoggedIn { get; private set; } = false;
+        private readonly ProtectedSessionStorage sessionStorage;
+
+        public bool IsLoggedIn { get; private set; } = false;
 		public string? UserName { get; private set; }
 		public Guid? EmployeeId { get; private set; }
 
 		public event Action? AuthStateChanged;
 
-		public void Login(string userName, Guid? employeeId)
+
+		public AuthStateService(ProtectedSessionStorage sessionStorage)
+        {
+            this.sessionStorage = sessionStorage;
+        }
+
+        public async Task Login(string userName, Guid employeeId)
 		{
 			IsLoggedIn = true;
 			UserName = userName;
 			EmployeeId = employeeId;
 
+			await sessionStorage.SetAsync(nameof(UserName), UserName);
+			await sessionStorage.SetAsync(nameof(EmployeeId), EmployeeId);
+
 			NotifyAuthStateChanged();
 		}
 
-		public void Logout()
+		public async Task Logout()
 		{
 			IsLoggedIn = false;
 			UserName = null;
 			EmployeeId = null;
 
-			NotifyAuthStateChanged();
+            await sessionStorage.DeleteAsync(nameof(UserName));
+            await sessionStorage.DeleteAsync(nameof(EmployeeId));
+
+            NotifyAuthStateChanged();
+		}
+
+		public async Task InitializeAuthState()
+		{
+			var userName = await sessionStorage.GetAsync<string>(nameof(UserName));
+			var employeeId = await sessionStorage.GetAsync<Guid>(nameof(EmployeeId));
+
+			if(userName.Success && employeeId.Success)
+			{
+				IsLoggedIn = true;
+				UserName = userName.Value;
+				EmployeeId = employeeId.Value;
+
+				NotifyAuthStateChanged();
+			}
 		}
 
 		private void NotifyAuthStateChanged()
